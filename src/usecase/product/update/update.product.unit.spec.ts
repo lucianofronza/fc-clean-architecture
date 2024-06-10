321110330368b15ffcc537a7b43ad41530f718e4
Luciano Fronza <luciano.fronza@gmail.com>
2024-06-10 11:53:19 -0300

    use cases "create", "find", "list", update" para product

---
+++ src/usecase/product/update/update.product.unit.spec.ts
@@ -0,0 +1,34 @@
+import ProductFactory from "../../../domain/product/factory/product.factory";
+import UpdateProductUseCase from "./update.product.usecase";
+
+const product = ProductFactory.create(
+    "a",
+    "Product A",
+    35
+);
+
+const input = {
+    id: product.id,
+    name: "Product A updated",
+    price: 35
+};
+
+const MockRepository = () => {
+    return {
+        create: jest.fn(),
+        findAll: jest.fn(),
+        find: jest.fn().mockReturnValue(Promise.resolve(product)),
+        update: jest.fn(),
+    };
+};
+
+describe("Unit test for product update use case", () => {
+    it("should update a product", async () => {
+        const productRepository = MockRepository();
+        const productUpdateUseCase = new UpdateProductUseCase(productRepository);
+
+        const output = await productUpdateUseCase.execute(input);
+
+        expect(output).toEqual(input);
+    });
+});
